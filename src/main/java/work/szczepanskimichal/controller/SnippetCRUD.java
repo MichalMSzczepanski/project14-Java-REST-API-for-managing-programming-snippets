ccc1965669b5dfbdd543ea92175b6cb4b5ea13b8
Mike <michal.m.szczepanski>
2021-06-21 18:32:31 +0200

    add api key validation

--- src/main/java/work/szczepanskimichal/controller/SnippetCRUD.java
+++ src/main/java/work/szczepanskimichal/controller/SnippetCRUD.java
@@ -18,50 +18,69 @@ import java.util.stream.Collectors;
 
 @WebServlet(name = "SnippetCRUD", value = "/snippetAPI")
 public class SnippetCRUD extends HttpServlet {
-
-
+    
     // json converter object has methods to parse json objects and objects
     JsonConverter jsonConverter = new JsonConverter();
     // for now initialize snippetList with two snippets (later communicate with DAO classes and DB)
     SnippetList snippetList = new SnippetList();
+    // static final api key for the time being
+    static final String apiKey = "123456789";
 
     // get all snippets or snippet by id
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getParameter("id") != null) {
-            // if parameter present - return specific snippet
-            String snippet = jsonConverter.convertObjectToJson(snippetList.getSnippetById(Integer.valueOf(request.getParameter("id"))));
-            response.getWriter().append(snippet);
-        } else {
-            // if parameter not present - return full list of snippets
-            String[] snippetArray = new String[snippetList.getSnippetList().size()];
-            for (int i = 0; i < snippetList.getSnippetList().size(); i++) {
-                snippetArray[i] = jsonConverter.convertObjectToJson(snippetList.getSnippetList().get(i));
+        if (request.getHeader("authentication").equals(apiKey)) {
+            if (request.getParameter("id") != null) {
+                // if parameter present - return specific snippet
+                String snippet = jsonConverter.convertObjectToJson(snippetList.getSnippetById(Integer.valueOf(request.getParameter("id"))));
+                response.getWriter().append(snippet);
+            } else {
+                // if parameter not present - return full list of snippets
+                String[] snippetArray = new String[snippetList.getSnippetList().size()];
+                for (int i = 0; i < snippetList.getSnippetList().size(); i++) {
+                    snippetArray[i] = jsonConverter.convertObjectToJson(snippetList.getSnippetList().get(i));
+                }
+                response.getWriter().append(Arrays.toString(snippetArray));
             }
-            response.getWriter().append(Arrays.toString(snippetArray));
+        } else {
+            response.getWriter().append("no api key found or api key doesn't exist");
         }
+
     }
 
     // post snippet
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        // tbc details why and how this works --->
-        String newSnippetJsonString = request.getReader().lines().collect(Collectors.joining());
-        // <---
-        snippetList.addSnippet(jsonConverter.convertJsonToSnippet(newSnippetJsonString));
-
+        if (request.getHeader("authentication").equals(apiKey)) {
+            // tbc details why and how this works --->
+            String newSnippetJsonString = request.getReader().lines().collect(Collectors.joining());
+            // <---
+            snippetList.addSnippet(jsonConverter.convertJsonToSnippet(newSnippetJsonString));
+        } else {
+            response.getWriter().append("no api key found or api key doesn't exist");
+        }
     }
 
     // update snippet by id
     protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String updatedSnippetJsonString = request.getReader().lines().collect(Collectors.joining());
-        snippetList.updateSnippetById(Integer.parseInt(request.getParameter("id")), jsonConverter.convertJsonToSnippet(updatedSnippetJsonString));
+
+        if (request.getHeader("authentication").equals(apiKey)) {
+            String updatedSnippetJsonString = request.getReader().lines().collect(Collectors.joining());
+            snippetList.updateSnippetById(Integer.parseInt(request.getParameter("id")), jsonConverter.convertJsonToSnippet(updatedSnippetJsonString));
+        } else {
+            response.getWriter().append("no api key found or api key doesn't exist");
+        }
     }
 
     // delete snippet by id
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        snippetList.deleteSnippet(Integer.parseInt(request.getParameter("id")));
+        if (request.getHeader("authentication").equals(apiKey)) {
+            snippetList.deleteSnippet(Integer.parseInt(request.getParameter("id")));
+        } else {
+            response.getWriter().append("no api key found or api key doesn't exist");
+        }
     }
+
+
 
 }
